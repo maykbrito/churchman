91ce8119eb4128ba639b67752ada27d379e260ab
Mayk Brito <maykbrito@gmail.com>
2019-03-08 13:35:01 -0300

    adding a public folder with index.html

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const db = require('./config/keys').mongoURI
 
 // Configure
 // ==============================================
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
