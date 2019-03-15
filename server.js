96c8ebc7ba47d66cc74119af5e97ac46ed4f5af0
Mayk Brito <maykbrito@gmail.com>
2019-03-15 00:19:16 -0300

    removing body-parser - updating packages

--- server.js
+++ server.js
@@ -1,37 +1,29 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
 const db = require('./config/keys').mongoURI
 
 
 // Configure
 // ==============================================
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());
 
 
 // DB
 // ==============================================
 mongoose
-.connect(db)
+.connect(db, { useNewUrlParser: true })
 .then(() => console.log("Mongo connected"))
 .catch(err => console.log("Mongo error: ", err))
 
 
 // ROUTES
 // ==============================================
-const churchs = require('./app/routes/api/churchs')
-const members = require('./app/routes/api/members')
-const events = require('./app/routes/api/events')
-const records = require('./app/routes/api/records')
-const ministries = require('./app/routes/api/ministries')
-
-app.use('/api/churchs', churchs)
-app.use('/api/members', members)
-app.use('/api/events', events)
-app.use('/api/records', records)
-app.use('/api/ministries', ministries)
+app.use('/api/churchs', require('./app/routes/api/churchs'))
+app.use('/api/members', require('./app/routes/api/members'))
+app.use('/api/events', require('./app/routes/api/events'))
+app.use('/api/records', require('./app/routes/api/records'))
+app.use('/api/ministries', require('./app/routes/api/ministries'))
 
 // RUN SERVER
 // ==============================================
