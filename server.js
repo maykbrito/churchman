461a2fe006761cae83746c1869354991a5869e95
Mayk Brito <maykbrito@gmail.com>
2019-05-11 12:54:43 -0300

    adding nodejs cors to allow dev

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+/* eslint-disable import/no-extraneous-dependencies */
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 
+const app = express();
+const mongoose = require('mongoose');
 
 // Configure
 // ==============================================
 app.use(express.json());
-
+app.use(cors({ origin: true }));
 
 // DB
 // ==============================================
 mongoose
-.connect(process.env.DB, { useNewUrlParser: true })
-.then(() => console.log("Mongo connected"))
-.catch(err => console.log("Mongo error: ", err))
-
+  .connect(process.env.DB, { useNewUrlParser: true })
+  .then(() => console.log('Mongo connected'))
+  .catch(err => console.log('Mongo error: ', err));
 
 // ROUTES
 // ==============================================
-app.use('/api/churchs', require('./app/routes/api/churchs'))
-app.use('/api/members', require('./app/routes/api/members'))
-app.use('/api/events', require('./app/routes/api/events'))
-app.use('/api/records', require('./app/routes/api/records'))
-app.use('/api/ministries', require('./app/routes/api/ministries'))
+app.use('/api/churchs', require('./app/routes/api/churchs'));
+app.use('/api/members', require('./app/routes/api/members'));
+app.use('/api/events', require('./app/routes/api/events'));
+app.use('/api/records', require('./app/routes/api/records'));
+app.use('/api/ministries', require('./app/routes/api/ministries'));
 
 // RUN SERVER
 // ==============================================
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(port, () => console.log(`Server started on port ${port}`));
